70485155d801fdd2fcf92194e0ca7817ea5f01a0
Jean-Francois James <jefrajames@gmail.com>
2019-02-15 15:59:23 +0100

    Minor change Signed-off-by:Jean-Francois James <jefrajames@gmail.com>

--- src/main/java/org/jefrajames/spqrdemo/SigninPayload.java
+++ src/main/java/org/jefrajames/spqrdemo/SigninPayload.java
@@ -20,7 +20,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * SigninPayload data provided after user signin.
+ * SigninPayload data returned on signin.
  * 
  * @author JF James
  */
